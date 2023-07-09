38949376a9ad1b7fafd47fbb63e91e432675c180
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-10 01:36:07 +0800

    add program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+double firstNumber = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double secondNumber = double.Parse(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+    Console.Write($"Число {firstNumber} больше чем число {secondNumber}");
+}
+else
+    if (firstNumber == secondNumber)
+    {
+        Console.Write($"Число {firstNumber} равно числу {secondNumber}");
+    }
+    else
+        Console.Write($"Число {secondNumber} больше чем число {firstNumber}");
